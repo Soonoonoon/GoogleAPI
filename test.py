86eba517b5d71c0fc5fc8daa404dbbfb0e6f2b12
Soonoonoon <55941557+Soonoonoon@users.noreply.github.com>
2020-11-04 19:28:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
 import gdapi
-import time
-
-start=time.time()
-
-id_='1BP0c4be04rqva7pSfrqGrmBhJwVN7lScxz5mBj3d6dk'
-sheet=gdapi.Writer(id_)
-count=1
-for i in range(0,100):
-        
-        range_='B'+str(count)
-      
-        sheet.write(range_,'Hello world')
-        count+=1
-        time.sleep(1)
-print(time.time()-start)
-ads+=1
 
 # if you want to create a new sheet
 
