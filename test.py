e18ee393b5f05b7b8181f078a653349d74b98036
Soonoonoon <55941557+Soonoonoon@users.noreply.github.com>
2020-11-05 20:48:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,67 +7,76 @@ Drive_1=gdapi.Drive("PICKLE PATH OR JSON PATH") # you can set another Drive2 ,et
 # use >> Drive_1.chose_json(path) or Drive_1.chose_pickle(path)
 # if you want to create a new sheet
 
-file_id=Drive_1.create_newsheet("new_sheet_name") # get a name of new_sheet_name Sheet
+## Change Crendential
+    # You can change different Drive to use or use more than 1 drive at the same time
+    
+    Drive.chose_json(jsonpath)
+    Drive.chose_pickle(picklepath)    
+## Download
+    Drive.download("filename","destination of computer")
+## Upload
+    Drive.upload("filename","destination of drive")
+## Delete
+    Drive.delete("filename")
+## Empty trash
+    Drive.emptytrash()
+## Find file
+    match,similar=Drive_1.find_file(filename) # return 2 list, match = equal filename , similar= similar to filename
+## Find folder
+    folder_id=Drive_1.find_folder_id(folder)  # return Folder id
+## Create new sheet
+    Drive.create_newsheet('sheetname')
+## Change permissions
+    Link=Drive.change_permissions(file_id) # change permission to anyone can write and read , return a webViewLink
+## Get webViewLink  
+    webViewLink=Drive.get_weblink(file_id)
 
-# change permission
-
-Drive_1.change_permissions(file_id) #
-# upload
-Drive_1.upload(filename,dstfolder) # dstfolder can also set a filepath or not set
-
-# download
-Drive_1.download(filename,dstfolder) # dstfolder can also set a filepath
-
-# delete
-Drive_1.delete(filename,dstfolder) # dstfolder can also set a filepath 
-
-# Write Sheet
-sheet=Drive_1.Writer(file_id)
-
-sheet.write('B5','In new_sheet_name, Hi this is B5')
-sheet.write("workbookname!A1:L1",['xuyy','xuyy','xuyy','xuyy','xuyy','xuyy',])#workbookname namechose
-
-# Write lots of
-arr=['abc','def','g','ad','ee','bb']
-sheet.write("A1:L1",arr) 
-
-# If write across row data
-
-arr=['abc','def','g','ad','ee','bb']
-
-for i in range(0,len(arr)):
-    sheet.write("A"+str(i),arr[i])
-    # A1 = abc
-    # A2 = def
-    # A3 = g
-    # A4 = ad
 
     
-# If write across row data method2
+## Sheet
+    sheet=Drive.Writer(sheetid)  # if no sheetid it will create a newsheet 
+    
+    Data='Hi'
+    sheet.write("A1",Data)  # A1='Hi'
+    Data=[['Hi','I'],['am','Test']]
+    sheet.write("A1:B2",Data)    # A1='Hi'  |  B1='I'
+                                 # A2='am'  |  B2='Test'
+    
+    #if set 1 behind Data 
+    sheet.write("A1:B2",Data,1)  # A1='Hi'  |  B1='am'
+                                 # A2='I'   |  B2='Test'
+    Data=[1,2,3,4,5,6]                           
+    sheet.write("A1:F1",Data)    # A1=1  |  B1=2 |  C1=3 |  D1=4 |  E1=5 |  F1=6 |
 
-arr=[['abc','def','g'],['ad','ee','bb']]
+    # If write across row data
 
+    arr=['abc','def','g','ad','ee','bb']
 
-sheet.write("A1:C2",arr)
-    # A1 = abc  B1=def C1=g
-    # A2 = def  B2=ee  C2=bb
+    for i in range(0,len(arr)):
+        sheet.write("A"+str(i),arr[i])
+        # A1 = abc
+        # A2 = def
+        # A3 = g
+        # A4 = ad
 
-
-# if write down in one column
-sheet.write('A2:A3',['abb','cc'],1) # set 1 behind data = wirte down along column
-        
-# Create sheet method 2
-sheet=gdapi.Writer
-sheet.create("filename","workbook1","workbook2")# create a [filename ]sheet, workbook1 , workbook2, will under the page ,be newpage
-
-# add new workbook in exist sheet
-sheet.add_sheet("workbook1","workbook2")
-
-# rename spreadsheet
-
-sheet.title="newtitle"
-
-# rename workbookdsheet
-
-sheet.resubtitle("oldtitle","newtitle")
-
+    
+    # Add new workbook in exist spreadsheet
+    sheet.add_sheet("workbook1","workbook2")
+    
+    # get spreadsheet name
+    sheet.title # return spreadsheet name
+    
+    # get spreadsheet id
+    sheet.id # return sheet id
+    
+    # get spreadsheet url
+    sheet.url # return sheet url
+    
+    # get spreadsheet sub
+    sublist=sheet.sub # return sheet sub list
+    
+    # rename spreadsheet
+    sheet.title="newtitle"
+    
+    # rename workbookdsheet
+    sheet.resubtitle("oldtitle","newtitle")
