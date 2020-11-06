2651f4a95a2a3f72e1a3f0013e033de590bf1b31
Soonoonoon <55941557+Soonoonoon@users.noreply.github.com>
2020-11-06 22:44:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import gdapi
 import time
-# Drive_1=gdapi.Drive(r'D:\Python\All_Practice\GoogleAPI\token\mnbbtoken.pickle')
+# Drive=gdapi.Drive(r'D:\Python\GoogleAPI\token\your.pickle')
 # Before ues API , log in your drive first.
 # Login Function:
-Drive_1=gdapi.Drive("PICKLE PATH OR JSON PATH") # you can set another Drive2 ,etc...
+Drive=gdapi.Drive("PICKLE PATH OR JSON PATH") # you can set another Drive2 ,etc...
 
-# if you want to change Drive_1 variable to use another Credential
-# use >> Drive_1.chose_json(path) or Drive_1.chose_pickle(path)
+# if you want to change Drive variable to use another Credential
+# use >> Drive.chose_json(path) or Drive.chose_pickle(path)
 # if you want to create a new sheet
-if Drive_1:
+if Drive:
 
     
 ## Change Crendential
@@ -31,11 +31,23 @@ if Drive_1:
 ## List all file
     Drive.list_all_file(view=1)# (self,*args,**kwargs) parameters: view=0/1 (list the found file , default 1)
 ## Find file
-    dict_of_find=Drive_1.find_file(filename)   # return a dict
+    dict_of_find=Drive.find_file(filename)   # return a dict
 ## Find folder
-    folder_id=Drive_1.find_folder_id(folder)   # return Folder id
-## Create new sheet
+    folder_id=Drive.find_folder_id(folder)   # return Folder id
+## Create NewFolder
+    Drive.mkdir(foldername) 
+## Create new Sheet
     Drive.create_newsheet('sheetname')
+    
+## Create new doc
+    Drive.create_doc('Docname')
+    
+## Create new Slide
+    Drive.create_Slides('Slidename')
+    
+## Create new Form
+    Drive.create_form('Formname')
+    
 ## Change permissions
     Link=Drive.change_permissions(file_id)     # change permission to anyone can write and read , return a webViewLink
 ## Get webViewLink  
@@ -50,28 +62,35 @@ if Drive_1:
     sharestate=Drive.get_shared(file_id)       # return boolean True / False
 ## Get Filelist
     Drive.get_filelist()                       # will write a list csv to default path
-## Sheet #!!! before use the sheet api , you need to login first ( >>Drive=gdapi.Drive("PICKLE PATH OR JSON PATH") )
-    sheet=gdapi.Sheet(sheetid)  # if no sheetid it will create a newsheet 
+## Sheet #   Before use the sheet api , you need to login first ( >>Drive=gdapi.Drive("PICKLE PATH OR JSON PATH") )
+    sheet=gdapi.Sheet(sheetid)  # if no sheetid it will create a newsheet
+
     # Sheet create
     sheet=gdapi.Sheet(name="NewTitle")  # it will create a new create which is named NewTitle 
-    
+
+
     # Write
-    
     Data='Hi'
-    sheet.write("A1",Data)  # A1='Hi'
+    sheet.write("A1",Data)       # A1='Hi'
     Data=[['Hi','I'],['am','Test']]
     sheet.write("A1:B2",Data)    # A1='Hi'  |  B1='I'
                                  # A2='am'  |  B2='Test'
-    
+    # Write with Cell :
+    sheet.write("1:4",Data)
+    #sheet.write("A1:B2",Data) == sheet.write("1:4",Data)
+
+
+
+    # Write cell
+    sheet.write("1:2",Data)
     #if set 1 behind Data 
     sheet.write("A1:B2",Data,1)  # A1='Hi'  |  B1='am'
                                  # A2='I'   |  B2='Test'
     Data=[1,2,3,4,5,6]                           
     sheet.write("A1:F1",Data)    # A1=1  |  B1=2 |  C1=3 |  D1=4 |  E1=5 |  F1=6 |
-    # read sheet
-    string=sheet.read("A1")      # return string
-    list_value=sheet.read("workbook2!:A1:B3")   # return Value list
-    # If write across row data
+    
+    
+    # Write across row data
 
     arr=['abc','def','g','ad','ee','bb']
 
@@ -82,24 +101,55 @@ if Drive_1:
         # A3 = g
         # A4 = ad
 
+    # Read sheet content
     
+    string=sheet.read("A1")                      # return string
+    list_value=sheet.read("workbook2!:A1:B3")    # return Value list
+
+    # Delete content
+    sheet.delete("A1:F1")    #A1~F1 will be deleted
+
+    # Color cell
+    sheet.setcolor("A1",(255,120,0),,) # (RangeSetColor,Tuple(R,G,B),HexColor,Alpha=0.5,SheetId=0)
+    #              Description                  |   Accept  Value
+    #----------------------------------------------------------------
+    #  RangeSetColor = set color on which cell  | Set  > "1:1"= "A1"
+    #  Tuple(R,G,B)  = set color RGB            | Set  > (255,255,0) # accept tuple and int
+    #　HexColor      = set color in Hex         | Set  > #CAFFFF    , if set Hexcolor , (R,G,B) can put anything except tuple.
+    #  Alpha         = set transparent          | Set  > [0,1]      , default=1
+    #  SheetId       = set sheetid              | Set  > long int   , default=0  , the number  in url gid=[sheetid] 
+
+    # Color method example2
+    sheet.setcolor("A1",0,'#CAFFFF',)
+
+    # Color method example3 : change to workbook3 
+    sheet.setcolor("A1",0,'#CAFFFF',sheetid=3)
+                   
     # Add new workbook in exist spreadsheet
     sheet.add_sheet("workbook1","workbook2")
     
-    # get spreadsheet name
+    # Get spreadsheet name
     sheet.title # return spreadsheet name
     
-    # get spreadsheet id
+    # Get spreadsheet id
     sheet.id # return sheet id
     
-    # get spreadsheet url
+    # Get spreadsheet url
     sheet.url # return sheet url
     
-    # get spreadsheet sub
+    # Get spreadsheet sub
     sublist=sheet.sub # return sheet sub list
     
-    # rename spreadsheet
+    # Rename spreadsheet
     sheet.title="newtitle"
     
-    # rename workbookdsheet
+    # Rename workbookdsheet
     sheet.resubtitle("oldtitle","newtitle")
+
+    # Adjust_column and row
+    Colrange="1:5" # Column A-D =1~4
+    Col_pixel= 20  # 20 pixels between  column 
+    Rowrange="1:4" # Row= 1~3
+    Row_pixel= 40  # 40 pixels between  row 
+    sheetid=0      # default=0
+    sheet.adjust_column_row(Colrange,Col_pixel,Rowrange,Row_pixel,*sheetid):
